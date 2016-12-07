50b7c6d67b487c1c0649bde02c028856d47bd766
Mike Swartz <mikelswartz@gmail.com>
2016-12-07 13:25:15 -0500

    Reconfigured for a more up to date version of git on 6.8

--- provision.sh
+++ provision.sh
@@ -2,4 +2,19 @@
 cd /vagrant/
 yum update -y
 yum upgrade -y
-yum install -y vim git
+yum install wget vim -y
+yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel -y
+yum install  gcc perl-ExtUtils-MakeMaker -y
+if [ -f /vagrant/git-2.8.4.tar.gz ]
+then
+	echo git-2.8.4.tar.gz exisits
+else
+	cd /vagrant/
+	wget https://www.kernel.org/pub/software/scm/git/git-2.8.4.tar.gz
+fi
+yum remove git -y 
+tar -xzf /vagrant/git-2.8.4.tar.gz -C /usr/src/
+cd /usr/src/git-2.8.4/
+make prefix=/usr/local/git all
+make prefix=/usr/local/git install
+echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
